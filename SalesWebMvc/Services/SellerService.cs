f97bba734c4d432812c233aa14218b7fd3ca8140
Lucas Mendes Israel <lukasm.israel@gmail.com>
2024-10-19 18:06:11 -0300

    04 - modificado index do Seller para exibir tabela e criado o SellerService

---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,20 @@
+﻿using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly Contexto _context;
+
+        public SellerService(Contexto context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
