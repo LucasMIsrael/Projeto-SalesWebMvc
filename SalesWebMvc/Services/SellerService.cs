407b9a6bcf987f94999ecf958eeeaaf5b72020d5
Lucas Mendes Israel <lukasm.israel@gmail.com>
2024-10-26 19:47:41 -0300

    05 - criado view create pro sellers e método no serviço e controlador

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,5 +1,7 @@
 ﻿using SalesWebMvc.Data;
 using SalesWebMvc.Models;
+using System.Diagnostics.Metrics;
+using System.Linq.Dynamic.Core;
 
 namespace SalesWebMvc.Services
 {
@@ -15,6 +17,21 @@ namespace SalesWebMvc.Services
         public List<Seller> FindAll()
         {
             return _context.Seller.ToList();
+        }
+
+        public void Insert(Seller obj)
+        {
+            obj.Departament = _context.Departament.First();
+
+            var lastSellerId = _context.Seller.OrderByDescending(x => x.Id).FirstOrDefault();
+
+            if (lastSellerId != null)
+                obj.Id = lastSellerId.Id + 1;
+
+            obj.BirthDate = DateTime.SpecifyKind(obj.BirthDate, DateTimeKind.Utc);
+
+            _context.Add(obj);
+            _context.SaveChanges();
         }
     }
 }
