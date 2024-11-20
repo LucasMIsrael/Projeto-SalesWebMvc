959220297e4bd818c9849bb69050a75f60b0b515
Lucas Mendes Israel <lukasm.israel@gmail.com>
2024-11-20 16:45:06 -0300

    13 - implementação do grouping search das vendas

--- SalesWebMvc/Services/SalesRecordsService.cs
+++ SalesWebMvc/Services/SalesRecordsService.cs
@@ -35,5 +35,34 @@ namespace SalesWebMvc.Services
                         .OrderByDescending(x => x.Date)
                         .ToListAsync();
         }
+
+        public async Task<List<IGrouping<Departament, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var query = _context.SalesRecord.AsQueryable();
+
+            if (minDate.HasValue)
+            {
+                minDate = DateTime.SpecifyKind((DateTime)minDate, DateTimeKind.Utc);
+                query = query.Where(x => x.Date >= minDate.Value);
+            }
+
+            if (maxDate.HasValue)
+            {
+                maxDate = DateTime.SpecifyKind((DateTime)maxDate, DateTimeKind.Utc);
+                query = query.Where(x => x.Date <= maxDate.Value);
+            }
+
+            var salesRecords = await query
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Departament)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+
+            var groupedResult = salesRecords
+                .GroupBy(x => x.Seller.Departament)
+                .ToList();
+
+            return groupedResult;
+        }
     }
 }
